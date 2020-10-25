1939a0b164e291a48c63d1a1c295231627e139b8
XIAC <x.i.a.c.1504@gmail.com>
2020-10-25 16:17:49 -0400

    Aplicacion de corrutinas

--- TallerAplicacionesInternet/Assets/Scripts/GenerarCubos.cs
+++ TallerAplicacionesInternet/Assets/Scripts/GenerarCubos.cs
@@ -6,18 +6,26 @@ public class GenerarCubos : MonoBehaviour
 {
     public GameObject cubos;
     public float frecuencia = 0.5f;
-    private float siguiente = 0.0f;
+    // private float siguiente = 0.0f;
     void Start()
     {
+        StartCoroutine(LanzarCubo());
+    }   
+    IEnumerator LanzarCubo(){
+        yield return new WaitForSeconds(2.0f);
         
+        while(true){
+            Instantiate(cubos, transform.position, Random.rotation);
+            yield return null;//retorno parcial 
+            yield return new WaitForSeconds(frecuencia);
+        }
     }
-
     // Update is called once per frame
     void Update()
     {
-       if (Time.time > siguiente){
-            siguiente = Time.time + frecuencia;
-            Instantiate(cubos, transform.position, Random.rotation);
-       } 
+    //    if (Time.time > siguiente){
+    //         siguiente = Time.time + frecuencia;
+    //         Instantiate(cubos, transform.position, Random.rotation);
+    //    } 
     }
 }
